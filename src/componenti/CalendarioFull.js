0c623492690e2c7e282af410e1fd7d0a7bf4b160
Raffaele Cirillo <raffaele@MacBookPro.station>
2025-06-14 10:26:07 +0200

    v1.0.5

--- src/componenti/CalendarioFull.js
+++ src/componenti/CalendarioFull.js
@@ -4,9 +4,9 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
 
-//import '@fullcalendar/common/main.css';
-import '@fullcalendar/daygrid/index.css';
-import '@fullcalendar/timegrid/index.css';
+import '@fullcalendar/common/main.css';
+import '@fullcalendar/daygrid/main.css';
+import '@fullcalendar/timegrid/main.css';
 
 const CalendarioFull = () => {
   const [lezioni, setLezioni] = useState([]);
