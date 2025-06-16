7d80e32d6588dc5a147e5b111f815d75e49cdb11
Raffaele Cirillo <raffaele@MacBookPro.station>
2025-06-16 22:03:27 +0200

    v1.7.4

--- src/componenti/CalendarioFull.js
+++ src/componenti/CalendarioFull.js
@@ -13,6 +13,22 @@ const CalendarioFull = ({ lezioni }) => {
     navigate(`/lezioni/${lezioneId}/modifica`);
   };
 
+  // 👉 Colora gli eventi in base allo stato
+  const eventDidMount = (info) => {
+    const stato = info.event.extendedProps.stato;
+
+    if (stato === 'rimandata') {
+      info.el.style.backgroundColor = 'orange';
+      info.el.style.color = 'white';
+    } else if (stato === 'annullata') {
+      info.el.style.backgroundColor = 'red';
+      info.el.style.color = 'white';
+    } else if (stato === 'svolta') {
+      info.el.style.backgroundColor = 'green';
+      info.el.style.color = 'white';
+    }
+  };
+
   return (
     <div className="p-4">
       <h2 className="text-xl font-bold mb-4">Calendario Lezioni</h2>
@@ -22,7 +38,8 @@ const CalendarioFull = ({ lezioni }) => {
         editable={false}
         selectable={true}
         events={lezioni}
-        eventClick={handleEventClick} // <-- aggiunto!
+        eventClick={handleEventClick}
+        eventDidMount={eventDidMount} // <-- AGGIUNTO QUI
         slotMinTime="08:00:00"
         slotMaxTime="22:00:00"
         locale="it"
@@ -35,4 +52,5 @@ const CalendarioFull = ({ lezioni }) => {
 };
 
 export default CalendarioFull;
+
 
