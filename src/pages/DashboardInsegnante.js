3464a1d52467a96605a094234fa29e45b9bc7b63
Raffaele Cirillo <raffaele@MacBookPro.station>
2025-07-12 13:32:17 +0200

    v1.11.0

---
+++ src/pages/DashboardInsegnante.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DashboardInsegnante = () => {
+  return (
+    <div>
+      <h2>Dashboard Insegnante</h2>
+      <p>Benvenuto! Qui troverai il tuo calendario, i tuoi allievi e i tuoi pagamenti.</p>
+    </div>
+  );
+};
+
+export default DashboardInsegnante;
