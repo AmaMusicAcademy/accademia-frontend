f6355376154109c7a5ca7b5aff73eb8efd22c202
Raffaele Cirillo <raffaele@MacBookPro.station>
2025-07-17 23:23:36 +0200

    v1.14.1

--- src/App.js
+++ src/App.js
@@ -1,36 +1,3 @@
-/*import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import NuovaLezione from './NuovaLezione';
-import ModificaLezione from './componenti/ModificaLezione';
-import CalendarioLezioniWrapper from './CalendarioLezioniWrapper'; // ✅ nuovo wrapper
-import Home from './Home';
-import Allievi from './Allievi';
-
-function App() {
-  return (
-    <Router>
-      <div style={{ maxWidth: 800, margin: 'auto', padding: 20 }}>
-        <nav style={{ marginBottom: 20 }}>
-          <Link to="/" style={{ marginRight: 10 }}>🏠 Home</Link>
-          <Link to="/nuova-lezione" style={{ marginRight: 10 }}>➕ Nuova Lezione</Link>
-          <Link to="/allievi">🎓 Gestione Allievi</Link>
-        </nav>
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/nuova-lezione" element={<NuovaLezione />} />
-          <Route path="/lezioni/:id/modifica" element={<ModificaLezione />} />
-          <Route path="/lezioni/:idInsegnante" element={<CalendarioLezioniWrapper />} />
-          <Route path="/allievi" element={<Allievi />} />
-        </Routes>
-      </div>
-    </Router>
-  );
-}
-
-export default App;
-*/
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
 import NuovaLezione from './NuovaLezione';
@@ -47,6 +14,7 @@ import CambiaPassword from './pages/CambiaPassword';
 import CambiaAvatar from './pages/CambiaAvatar';
 import CalcoloRimborso from './pages/CalcoloRimborso';
 import CalendarioPersonale from './pages/CalendarioPersonale';
+import AdminDashboard from './pages/AdminDashboard';
 
 
 function Layout({ children }) {
@@ -84,6 +52,7 @@ function App() {
   <Route path="/profilo/password" element={<CambiaPassword />} />
   <Route path="/cambia-avatar" element={<CambiaAvatar />} />
   <Route path="/rimborso" element={<CalcoloRimborso />} />
+  <Route path="/admin" element={<AdminDashboard />} />
 </Routes>
 
   );
