952994792b60ff07f1b2260286e34dab19f68dbd
Raffaele Cirillo <raffaele@MacBookPro.station>
2025-07-18 21:00:34 +0200

    v1.14.3

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import CambiaPassword from './pages/CambiaPassword';
 import CambiaAvatar from './pages/CambiaAvatar';
 import CalcoloRimborso from './pages/CalcoloRimborso';
 import CalendarioPersonale from './pages/CalendarioPersonale';
-import AdminDashboard from './pages/AdminDashboard';
+//import AdminDashboard from './pages/AdminDashboard';
 import AdminPage from './pages/AdminPage';
 
 
@@ -54,7 +54,7 @@ function App() {
   <Route path="/profilo/password" element={<CambiaPassword />} />
   <Route path="/cambia-avatar" element={<CambiaAvatar />} />
   <Route path="/rimborso" element={<CalcoloRimborso />} />
-  <Route path="/admin" element={<AdminDashboard />} />
+
   <Route path="/admin" element={<AdminPage />} />
 </Routes>
 
