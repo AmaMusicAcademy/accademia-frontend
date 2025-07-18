be044d5eae1bcc77b9cf9cd1a8285288ff159c25
Raffaele Cirillo <raffaele@MacBookPro.station>
2025-07-18 23:00:15 +0200

    v1.14.5

--- src/pages/ProfiloAdmin.js
+++ src/pages/ProfiloAdmin.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import BottomNavAdmin from '../components/BottomNavAdmin';
+import BottomNavAdmin from '../componenti/BottomNavAdmin';
 
 const ProfiloAdmin = () => {
   const navigate = useNavigate();
