5bb5f08190c057564c6191e5913c0df105fab877
Raffaele Cirillo <raffaele@MacBookPro.station>
2025-07-25 20:07:51 +0200

    v1.15.7

--- src/pages/DettaglioInsegnante.js
+++ src/pages/DettaglioInsegnante.js
@@ -46,7 +46,6 @@ const DettaglioInsegnante = () => {
   const infoRows = [
     ['Nome', insegnante.nome],
     ['Cognome', insegnante.cognome],
-    ['Username', insegnante.username],
     ['ID', insegnante.id]
   ];
 
@@ -74,18 +73,20 @@ const DettaglioInsegnante = () => {
 
         {/* Allievi assegnati */}
         {allievi.length > 0 && (
-          <div className="bg-white rounded-xl shadow mt-6 divide-y text-sm">
-            <div className="px-4 py-3 font-semibold text-gray-700">
-              Allievi assegnati
-            </div>
-            {allievi.map((a) => (
-              <div key={a.id} className="px-4 py-3 text-gray-800">
-                {a.nome} {a.cognome}
-              </div>
-            ))}
-          </div>
-        )}
+  <div className="bg-white rounded-xl shadow mt-6 divide-y text-sm">
+    <div className="flex justify-between px-4 py-3 font-semibold text-gray-700">
+      <span>Allievi assegnati</span>
+      <span className="text-gray-400">{allievi.length}</span>
+    </div>
+    {allievi.map((a) => (
+      <div key={a.id} className="flex justify-between px-4 py-3">
+        <span className="text-gray-600">Allievo</span>
+        <span className="text-gray-800 text-right">{a.nome} {a.cognome}</span>
       </div>
+    ))}
+  </div>
+)}
+
 
       {/* BottomBar con tasto "Modifica" centrale */}
       <BottomNavAdmin showEditButton onEdit={() => navigate(`/admin/insegnanti/${id}/modifica`)} />
