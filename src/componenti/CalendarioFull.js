b513f91db89d2e14fd28c24eb77102a8f2b3f16d
Raffaele Cirillo <raffaele@IPHONE.station>
2025-08-31 19:55:55 +0200

    v1.20.10

--- src/componenti/CalendarioFull.js
+++ src/componenti/CalendarioFull.js
@@ -268,7 +268,7 @@ export default function CalendarioFull({ lezioni }) {
 
             /* calendario leggermente più basso ma con tutte le settimane */
             height="auto"
-            contentHeight="auto"
+            contentHeight={360}
             expandRows={true}
             fixedWeekCount={true}
             handleWindowResize={true}
